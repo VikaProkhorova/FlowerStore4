ed6713ed658d166375baaff2d05174b7e6cf67d5
VikaProkhorova <116552538+VikaProkhorova@users.noreply.github.com>
2024-01-08 13:50:05 +0200

    Delete src directory

--- src/main/java/ua/edu/ucu/apps/flowerstore/delivery/Delivery.java
+++
@@ -1,8 +0,0 @@
-package ua.edu.ucu.apps.flowerstore.delivery;
-import ua.edu.ucu.apps.flowerstore.store.Item;
-
-import java.util.List;
-
-public interface Delivery {
-    void deliver(List<Item>items);
-}
--- src/main/java/ua/edu/ucu/apps/flowerstore/store/Item.java
+++
@@ -1,9 +0,0 @@
-package ua.edu.ucu.apps.flowerstore.store;
-
-public abstract class Item {
-    private String description;
-    public abstract double getPrice();
-    public String getDescription(){
-        return description;
-    }
-}
